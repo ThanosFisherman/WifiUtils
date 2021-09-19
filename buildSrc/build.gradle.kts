ec4d278392cfd2b0034a1247faf6e56586245eee
Thanos Psaridis <athanasios.psaridis@uxma.com>
2021-09-19 16:12:20 +0300

    Prepare for maven publishing

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,11 +1,25 @@
 plugins {
-    base
+    `java-gradle-plugin`
     `kotlin-dsl`
 }
 
-allprojects {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    //google()
+    gradlePluginPortal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(gradleTestKit())
+    testImplementation("junit:junit:4.13.2")
+
+}
+
+tasks.withType<Test>().configureEach {
+    testLogging { showStandardStreams = true }
 }
--- buildSrc/src/main/kotlin/Artifact.kt
+++ buildSrc/src/main/kotlin/Artifact.kt
@@ -1,3 +1,7 @@
+import org.gradle.api.Project
+import java.util.*
+import kotlin.collections.LinkedHashMap
+
 object Artifact {
 
     val ARTIFACT_NAME = "wifiutils"
@@ -6,6 +10,8 @@ object Artifact {
     val VERSION_CODE = 22
     val POM_URL = "https://github.com/ThanosFisherman/WifiUtils"
     val POM_SCM_URL = "https://github.com/ThanosFisherman/WifiUtils"
+    val POM_SCM_CONNECTION = "scm:git:github.com/ThanosFisherman/WifiUtils.git"
+    val POM_SCM_DEV_CONNECTION = "scm:git:ssh://github.com/ThanosFisherman/WifiUtils.git"
     val POM_ISSUE_URL = "https://github.com/ThanosFisherman/WifiUtils/issues"
     val POM_DESC = "Library that makes it easy to Connect to WiFi hotspots using ssid and/or bssid"
 
@@ -21,4 +27,40 @@ object Artifact {
 
     val POM_DEVELOPER_ID = "thanosfisherman"
     val POM_DEVELOPER_NAME = "Thanos Psaridis"
+    val DEVELOPER_EMAIL = "psaridis@gmail.com"
+
+    val RELEASE_REPO_URL = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+    val SNAPSHOT_REPO_URL = "https://oss.sonatype.org/content/repositories/snapshots"
+    val REPO_NAME = "sonatype"
 }
+
+
+val Project.credentialsMap: Map<String, String>
+    inline get() =
+        LinkedHashMap<String, String>().apply {
+
+            val propertiesFile = rootProject.file("local.properties")
+            if (propertiesFile.exists() && propertiesFile.canRead()) {
+               try {
+                   val properties = Properties()
+                   properties.load(propertiesFile.inputStream())
+                   this["signing.keyId"] = properties.getProperty("signing.keyId")
+                   this["signing.password"] = properties.getProperty("signing.password")
+                   this["signing.secretKeyRingFile"] =
+                       properties.getProperty("signing.secretKeyRingFile")
+                   this["ossrhUsername"] = properties.getProperty("ossrhUsername")
+                   this["ossrhPassword"] = properties.getProperty("ossrhPassword")
+                   this["sonatypeStagingProfileId"] =
+                       properties.getProperty("sonatypeStagingProfileId")
+               } catch (e: Exception) {}
+            } else {
+                this["signing.keyId"] = System.getenv("signing.keyId")
+                this["signing.password"] = System.getenv("signing.password")
+                this["signing.secretKeyRingFile"] = System.getenv("signing.secretKeyRingFile")
+                this["ossrhUsername"] = System.getenv("ossrhUsername")
+                this["ossrhPassword"] = System.getenv("ossrhPassword")
+                this["sonatypeStagingProfileId"] = System.getenv("sonatypeStagingProfileId")
+                //val keystoreFile = project.rootProject.file(rootDir.path + File.separator + System.getenv("keystore_name"))
+            }
+        }
+
